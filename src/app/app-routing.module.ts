47e8bbb8f1fad2abae8d1542fc4828d10995fdc6
therese.rossi <therese.rossi@mail.com>
2019-05-15 12:41:18 +0200

    adding service and trying to connect to cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+// import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent}
