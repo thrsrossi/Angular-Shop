2dec685a1599650bc209ca6e683c9e477a8b4bc8
therese.rossi <therese.rossi@mail.com>
2019-05-21 18:08:55 +0200

    tests on input decorator to cart-aside-component passes

--- src/app/components/cart-container/cart-container.component.spec.ts
+++ src/app/components/cart-container/cart-container.component.spec.ts
@@ -4,28 +4,29 @@ import { CartContainerComponent } from './cart-container.component';
 import { PrintCartContainerComponent } from '../print-cart-container/print-cart-container.component';
 import { CartService } from 'src/app/services/cart.service';
 import { ICartItem } from 'src/app/interfaces/ICartItem';
+import { CartAsideComponent } from '../cart-aside/cart-aside.component';
 
 describe('CartContainerComponent', () => {
   let component: CartContainerComponent;
   let fixture: ComponentFixture<CartContainerComponent>;
-  let cartService: CartService;
-  let cartItem: ICartItem[] = [{
-        movie: {
-          id: 6,
-          name: "hej",
-          description: "string",
-          price: 589,
-          imageUrl: "imageUrl",
-          year: 8898,
-          added: "string",
-          productCategory: []
-      },
-      quantity: 5
-    }];
+//   let cartService: CartService;
+//   let cartItem: ICartItem[] = [{
+//         movie: {
+//           id: 6,
+//           name: "hej",
+//           description: "string",
+//           price: 589,
+//           imageUrl: "imageUrl",
+//           year: 8898,
+//           added: "string",
+//           productCategory: []
+//       },
+//       quantity: 5
+//     }];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CartContainerComponent, PrintCartContainerComponent ],
+      declarations: [ CartContainerComponent, PrintCartContainerComponent, CartAsideComponent ],
       providers: [ CartService ]
     })
     .compileComponents();
